65c1314a15ef8371fb2f971f7a84a7ea63db0c4e
Matt Brucker <mpbrucker@gmail.com>
2020-07-07 09:50:25 -0400

    Adding webpack dev server reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
+const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
 
 module.exports = {
-    entry: './src/boundary-sidebar.js',
+    context: __dirname,
+    entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new HtmlWebPackPlugin()
+    ]
 };
