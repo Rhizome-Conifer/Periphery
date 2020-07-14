2817e3e4d5fc6351a3f896b40e5ee60bb58da262
Matt Brucker <mpbrucker@gmail.com>
2020-07-14 14:29:29 -0400

    Added inline style error checking

--- test/mutator.test.js
+++ test/mutator.test.js
@@ -3,36 +3,18 @@ import { attachDivOverlay, inlineStyle } from '../src/mutator';
 export function mutatorTestRunner() {
     describe('mutates DOM and styles divs correctly', () => {
         let testParent = document.createElement('div');
-        const attachDiv = (className, description, styling) => {
-            return () => attachDivOverlay(testParent, className, description, styling);
-        }
+        document.body.appendChild(testParent);
     
-        test('attach overlay with incorrect property types', () => {
-            let expectedTemplate = [
-                '\n            <div class=',
-                '\n                style=',
-                '\n                data-after=',
-                '\n            ></div>\n        '
-            ];
-            let parent = document.createElement('div');
-            let overlay = attachDivOverlay(parent, ["Test", "Test"], {"Test": "test"}, "asdf");
-            expect(overlay.render().strings).toEqual(expectedTemplate);
-        });
-
         test('attach overlay with style map', () => {
-            let expectedTemplate = [
-                '\n            <div class=',
-                '\n                style=',
-                '\n                data-after=',
-                '\n            ></div>\n        '
-            ];
             let parent = document.createElement('div');
             let overlay = attachDivOverlay(parent, "test", {"color": "red"});
-            console.log(overlay.render().strings)
-            expect(overlay.render().strings).toEqual(expectedTemplate);
+            expect(parent.children.length).toEqual(1);
         });
 
-
-    
+        test('attach incorrectly-formatted style', () => {
+            let style = {"test": [1, 2, 3]};
+            let testStyle = () => inlineStyle(testParent, style, "*");
+            expect(testStyle).toThrow(Error);
+        })
     });
 }
