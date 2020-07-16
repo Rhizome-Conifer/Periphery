be64095d53407429f4dc00dc580c115fdcbbb227
Matt Brucker <mpbrucker@gmail.com>
2020-07-16 10:09:39 -0400

    Add electron testing and debug script

--- test/mutator.test.js
+++ test/mutator.test.js
@@ -5,16 +5,36 @@ export function mutatorTestRunner() {
         let testParent = document.createElement('div');
         document.body.appendChild(testParent);
     
-        test('attach overlay with style map', () => {
-            let parent = document.createElement('div');
-            let overlay = attachDivOverlay(parent, "test", {"color": "red"});
-            expect(parent.children.length).toEqual(1);
-        });
 
         test('attach incorrectly-formatted style', () => {
             let style = {"test": [1, 2, 3]};
             let testStyle = () => inlineStyle(testParent, style, "*");
             expect(testStyle).toThrow(Error);
         })
+    });
+
+    describe('overlay div correctly renders', () => {
+        const PARENT_ID = 'parent';
+        const getShadowRoot = (selector) => {
+            return document.querySelector(selector).shadowRoot;
+        }
+ 
+        beforeEach(() => {
+            let parent = window.document.createElement('div');
+            parent.id = PARENT_ID;
+            document.body.appendChild(parent);
+        });
+
+        afterEach(() => {
+            document.body.querySelector('div').remove();
+        });
+
+        test('attach overlay with style map', () => {
+            let parent = window.document.getElementById(PARENT_ID);
+            let overlay = attachDivOverlay(parent, "test", {"color": "red"});
+            let innerDiv = overlay.shadowRoot.querySelector('div');
+            expect(innerDiv.style.color).toEqual('red');
+        });
+
     });
 }
