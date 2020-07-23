622ac16b340d9a5f9eaf6323d0d6025fa67ec2a4
Matt Brucker <mpbrucker@gmail.com>
2020-07-23 13:42:48 -0400

    Adding host / endpoint as arguments to Pool constructor

--- src/worker-pool.js
+++ src/worker-pool.js
@@ -1,15 +1,42 @@
 export class Pool {
-    constructor(size, script) {
+    constructor(size, host, endpoint) {
         this.size = size;
-        this.script = script;
+        this.host = host;
+        this.endpoint = endpoint;
         this.taskQueue = [];
         this.workerQueue = this.init(size);
     }
 
     init(size) {
+        // Create the blob URL for the worker function
+        let workerFunc = `
+        function checkCdxQueryResult(uri) {
+            return fetch(uri).then
+            (res => res.text()).then
+            (response => response != '');
+        }
+        
+        onmessage = function(e) {
+            let host = e.data.host;
+            let endpoint = e.data.endpoint;
+            let href = e.data.href;
+            if (!href.startsWith('javascript')) {
+                let url = host + endpoint + "?output=json&limit=1&url=" + encodeURIComponent(href);
+                checkCdxQueryResult(url).then((isPresent) => {
+                    self.postMessage([href, isPresent]);
+                });
+            } else {
+                // for javascript() hrefs and other things that we know aren't within boundary
+                self.postMessage([href, false]);
+            }
+        };
+        `;
+        let blob = new Blob([workerFunc], { type: 'application/javascript' });
+        var url = URL.createObjectURL(blob);
+
         let workerThreads = [];
         for (let i=0;i<size;i++) {
-            workerThreads.push(new WorkerThread(this.freeThread.bind(this)));
+            workerThreads.push(new WorkerThread(this.host, this.endpoint, url, this.freeThread.bind(this)));
         }
         return workerThreads;
     }
@@ -46,34 +73,10 @@ export class Pool {
 }
 
 class WorkerThread {
-    constructor(freeThread) {
-        let workerFunc = `
-        function checkCdxQueryResult(uri) {
-            return fetch(uri).then
-            (res => res.text()).then
-            (response => response != '');
-        }
-        
-        onmessage = function(e) {
-            let host = e.data.host;
-            let endpoint = e.data.endpoint;
-            let href = e.data.href;
-            if (!href.startsWith('javascript')) {
-                let url = host + endpoint + "?output=json&limit=1&url=" + encodeURIComponent(href);
-                checkCdxQueryResult(url).then((isPresent) => {
-                    self.postMessage([href, isPresent]);
-                });
-            } else {
-                // for javascript() hrefs and other things that we know aren't within boundary
-                self.postMessage([href, false]);
-            }
-        };
-        `;
-
-        let blob = new Blob([workerFunc], { type: 'application/javascript' });
-        var url = URL.createObjectURL(blob);
-        this.worker = new Worker(url);
-        URL.revokeObjectURL(url);
+    constructor(host, endpoint, blobURL, freeThread) {
+        this.worker = new Worker(blobURL);
+        this.host = host;
+        this.endpoint = endpoint;
         this.freeThread = freeThread;
     }
 
@@ -82,7 +85,7 @@ class WorkerThread {
             task.callback(val);
             this.freeThread(this);
         }
-        this.worker.postMessage({host: task.message.host, endpoint: task.message.endpoint, href: task.message.href});
+        this.worker.postMessage({host: this.host, endpoint: this.endpoint, href: task.message.href});
     }
 }
 
