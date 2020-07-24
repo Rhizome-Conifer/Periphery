429af91350bee3d09e2628ab755d8b3ac9a8d4f7
Matt Brucker <mpbrucker@gmail.com>
2020-07-24 14:45:28 -0400

    Fix merge conflict in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
-const WorkerPlugin = require('worker-plugin');
 
 module.exports = {
     context: __dirname,
@@ -10,20 +9,6 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-<<<<<<< HEAD
-        new HtmlWebPackPlugin(),
-        new WorkerPlugin()
-    ]
-=======
         new HtmlWebPackPlugin()
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.worker\.js$/,
-                use: { loader: 'worker-loader' },
-            }
-        ]
-    }
->>>>>>> ec2b7cedcb617c90d54a73eb8a518e886b31a58e
+    ]
 };
